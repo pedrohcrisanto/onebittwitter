d841e633ed971e73ed9f8a32b9c8e04cef1cd25f
Pedro Crisanto <pedrohcrisanto@gmail.com>
2020-07-07 14:31:41 -0300

    Cancancan

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,9 @@ module Api
     module V1
       class ApiController < ApplicationController
         include Knock::Authenticable
+        include CanCan::ControllerAdditions
+      
         #> Métodos globais
       end
     end
-end
+  end
